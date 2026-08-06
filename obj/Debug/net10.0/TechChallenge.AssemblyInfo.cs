d6d48aec4b69e43336a4243b339629211129baeb
Natallie <amadeunatallie@gmail.com>
2026-08-06 11:23:18 -0300

    MO6- Estrtura inicial para Identidade

--- obj/Debug/net10.0/TechChallenge.AssemblyInfo.cs
+++ obj/Debug/net10.0/TechChallenge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechChallenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+187a1b513ec1dd2ef207a367e2b2fa001fce516c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04f704b284f9b0eca8efd2db6d72ab6eb6cd8b17")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechChallenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechChallenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
