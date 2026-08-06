c92f40038045efb9acb6583b90238c9df1a32bf6
Prof. Cristiano de Paula <prof.cristianodepaula@gmail.com>
2026-08-06 08:00:16 -0300

    Arquivos temporarios

--- obj/Debug/net10.0/TechChallenge.AssemblyInfo.cs
+++ obj/Debug/net10.0/TechChallenge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechChallenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5a35ad7fea21c3e56d22eb7b6c0da4d23f11122")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df6aa65b6b856e710545b4dc0578093190724a9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechChallenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechChallenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
