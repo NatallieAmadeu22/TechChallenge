187a1b513ec1dd2ef207a367e2b2fa001fce516c
Natallie <amadeunatallie@gmail.com>
2026-08-06 09:29:58 -0300

    SubindoBranch

--- obj/Debug/net10.0/TechChallenge.AssemblyInfo.cs
+++ obj/Debug/net10.0/TechChallenge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechChallenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df6aa65b6b856e710545b4dc0578093190724a9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cca54e368322a03448dcf1e485443aef2d4cb51d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechChallenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechChallenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
